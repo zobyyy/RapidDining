0ff132c35733444850d85a1e61252ea87fc87079
Zobyyy <codybabydd@gmail.com>
2023-08-14 22:56:59 +0800

    style(homepage): add homepage style

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,3 +1,15 @@
-export default function Home() {
-    return <h1>Hello, Next.js!</h1>
+import Layouts from '../../components/Layouts'
+import styles from './Home.module.scss'
+
+export default function Page() {
+  return (
+    <main>
+        <Layouts>
+            <div className={styles.Logo}>
+                <div>食</div>
+                <div>時好</div>
+            </div>
+        </Layouts>
+    </main>
+  )
 }
