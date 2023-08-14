b5aeb098573a6e81c5446876cc591985f7ad0f08
Zobyyy <codybabydd@gmail.com>
2023-08-14 13:33:09 +0800

    docs(eslint): fix eslint deploy error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 }
 
 module.exports = nextConfig
