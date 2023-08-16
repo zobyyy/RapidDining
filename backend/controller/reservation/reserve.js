56deb70335afb09d0dd9357c2b829cfab3914e62
yihsuan <yiiihsuann@gmail.com>
2023-08-16 23:34:06 +0800

    reservation API

--- backend/controller/reservation/reserve.js
+++ backend/controller/reservation/reserve.js
@@ -8,26 +8,7 @@
 //   throw new Error("not implemented");
 // }
 
-//funciton:  reserve a table, make reservation, waiting time
-// 我會從request body 得到的資料：
-// restaurantId	
-// headcount	
-// name	
-// gender	Enum<小姐 | 先生 | 其他>
-// phone	String	Phone number
-// 目前要做的功能：
-// 1.在該間餐廳該個人是否已存在候位或訂位資料(查詢Reservation中是否存在和輸入一樣的phone number || tableList 中是否存在和輸入一樣的phone number)
-// makeReservation
-// 2.如果該間餐廳是否有對應人數的空位(query tableList,在對應的restaurantId以及headcount>= headcount是否有vancancy=true存在，如果有的話就回傳true  )
-// 3.如果該間餐廳有對應人數的空位，則更新tableList (restaurantId, headcount, vacancy, phone) = (不變,不變,false,phone)
-// 4.如果該間餐廳沒有對應人數的空位，則將資料插入Reservation (phone, headcount, restaurantId) = (11111111, 2, 1)
-// 5.getWaitCount：候位第幾組：在Reservation 中對應的restaurantId 有幾個
-// 6.最後回傳的格式
-// data:{
-//   "tableId":
-//   "reservationId":
-//   "name":   
-//  }
+
 
 console.log("this is for reservation");
 
@@ -38,10 +19,6 @@ import {updateTableVacancy} from '../../model/reserveModel.js';
 import {checkExistingReservation} from '../../model/reserveModel.js'; 
 import {getWaitCount} from '../../model/reserveModel.js'; 
 
-
-
-
-//order contrl
 
 export async function reservationReserve(req, res) {
   try {
@@ -54,14 +31,10 @@ export async function reservationReserve(req, res) {
 
     console.log(typeof phone); 
 
-    const phoneNum = parseInt(phone, 10); // 轉換成整數
+    const phoneNum = parseInt(phone, 10); 
     console.log(phoneNum);
     console.log(typeof phoneNum);
 
-
-
-
-   
 
     console.log(`restaurant is ${restaurantId}`);
     console.log(`headcount is ${headcount}`);
@@ -92,7 +65,6 @@ export async function reservationReserve(req, res) {
         "reservationId": insertreservationId,
         "waitCount": reserveCount
       }});
-      //return res.status(200).json({ "message": 'successfully make a reservation' });
     }
     const updateTableId = await updateTableVacancy(phoneNum,headcount);
     console.log("Good, you already have a seat");
@@ -110,7 +82,7 @@ export async function reservationReserve(req, res) {
 }
 
 
-       //check if restaurant exist
+    //check if restaurant exist
     // const restaurant = await db.getRestaurantById(restaurantId); //query if restaurant is exist
     // if (!restaurant) {
     //   return res.status(404).json({ error: 'restaurant not found' });
