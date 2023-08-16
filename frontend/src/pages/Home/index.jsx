283b3fb85b40ad65fb8eef7b3e221655290fde39
Zobyyy <codybabydd@gmail.com>
2023-08-16 17:44:50 +0800

    feat(api): testAPI

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -12,7 +12,30 @@ import { useState } from 'react'
 export default function Home() {
   const [isHidden, setIsHidden] = useState(false);
   const [isSearch,setIsSearch] = useState(false);
+  const [data, setData] = useState(null);
   const bookingInfo = [false, true, false, true, false, true];
+
+  useEffect(() => {
+    // 定義一個 async 函數來執行 GET 請求
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          'http://ec2-3-84-189-230.compute-1.amazonaws.com'
+        )
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        // const responseData = await response.text()
+        setData(response)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    // 呼叫 fetchData 函數來執行 GET 請求
+    fetchData()
+  }, [])
+
 
   return (
     <main>
