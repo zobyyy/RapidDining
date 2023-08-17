0529069dfcfe609373aa76fc9d3b244649ea8e72
tsaichiehhuang <a0970605512@gmail.com>
2023-08-17 21:13:47 +0800

    fix:menu跳轉修改

--- frontend/src/pages/Booking/Menu.js
+++ frontend/src/pages/Booking/Menu.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import styles from './Booking.module.scss'
 import Image from 'next/image'
 import ProductColumn from './ProductColumn'
@@ -63,7 +63,35 @@ const mockData = {
         type: '咖啡',
         product: [
           {
-            id: 4,
+            id: 6,
+            name: '拿鐵特調',
+            price: 295,
+            description: '濃郁咖啡香，與絲滑牛奶交織，絕佳口感',
+            picture: '<https://lattee.com/XXXXX>',
+            spicy: false,
+            vegan: false
+          }
+        ]
+      },
+      {
+        type: '其他飲品',
+        product: [
+          {
+            id: 7,
+            name: '拿鐵特調',
+            price: 295,
+            description: '濃郁咖啡香，與絲滑牛奶交織，絕佳口感',
+            picture: '<https://lattee.com/XXXXX>',
+            spicy: false,
+            vegan: false
+          }
+        ]
+      },
+      {
+        type: '甜點',
+        product: [
+          {
+            id: 8,
             name: '拿鐵特調',
             price: 295,
             description: '濃郁咖啡香，與絲滑牛奶交織，絕佳口感',
@@ -80,20 +108,37 @@ const mockData = {
 }
 
 const Menu = () => {
-  const kindsRefs = mockData.data.menu.map(() => useRef(null))
-  const scrollToKinds = (index) => {
-    kindsRefs[index].current.scrollIntoView({
-      behavior: 'smooth'
+  const [activeKindIndex, setActiveKindIndex] = useState(0)
+  const kindsRefs = mockData.data.menu.map(() => useRef(null)) // 使用 map 創建 useRef 陣列
+
+  useEffect(() => {
+    // 在這裡設定各個 refs 的值
+    kindsRefs.forEach((ref, index) => {
+      kindsRefs[index] = ref // 這裡將 ref 賦值為 DOM 元素
     })
+  }, [kindsRefs])
+
+  const scrollToKinds = (index) => {
+    if (kindsRefs[index].current) {
+      kindsRefs[index].current.scrollIntoView({
+        behavior: 'smooth'
+      })
+    }
+  }
+  const handleKindClick = (index) => {
+    setActiveKindIndex(index)
+    scrollToKinds(index)
   }
   const Kinds = () => {
     return (
-      <div className={styles.scrollContainer}>
+      <div className={styles.kindsContainer}>
         {mockData.data.menu.map((type, index) => (
           <div
             key={index}
-            className={styles.kindsItem}
-            onClick={() => scrollToKinds(index)}
+            className={`${styles.kindsItem} ${
+              activeKindIndex === index ? styles.activeKinds : ''
+            }`}
+            onClick={() => handleKindClick(index)}
           >
             {type.type}
           </div>
