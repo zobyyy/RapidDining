95bef05195512b77998b10be2e3da4814680f147
yihsuan <yiiihsuann@gmail.com>
2023-08-17 10:42:10 +0800

    add return and modify comment

--- backend/controller/menu/detail.js
+++ backend/controller/menu/detail.js
@@ -1,17 +1,15 @@
+import {getMenuDetail} from '../../model/dishdetailModel.js'; 
+
 /**
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  * */
-
-
-import {getMenuDetail} from '../../model/dishdetailModel.js'; 
-
 export async function dishDetail(req, res) {
   try {
     const dishId = req.params.dishId;
     const dish = await getMenuDetail(dishId); 
-    res.status(200).json({ data: dish });
+    return res.status(200).json({ data: dish });
   } catch (error) {
     console.error('Error in reservationReserve:', error);
     return res.status(500).json({ error: 'internal server error' });
