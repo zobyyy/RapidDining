989ed2f569e148763a8626b267c6d784c8a39b3c
Zobyyy <codybabydd@gmail.com>
2023-08-18 11:54:51 +0800

    feat(infinitescroll): success detect layout scroll height

--- frontend/src/components/Layouts/index.js
+++ frontend/src/components/Layouts/index.js
@@ -3,12 +3,21 @@ import styles from './Layouts.module.scss'
 
 export default function Layouts({ children, scrollBarHidden }) {
     const layoutRef = useRef(null);
-    const [isAtBottom, setIsAtBottom] = useState(false);
+    const [isBottom, setIsBottom] = useState(false);
 
     const handleScroll = () => {
-        const container = containerRef.current;
-        if (layoutRef.current) {
-            console.log("scroll")
+        const layoutElement = layoutRef.current;
+
+        const total = layoutElement.scrollTop + layoutElement.clientHeight; 
+        console.log("isBottom: "+ isBottom)
+        console.log("scrollHeight: "+(layoutElement.scrollHeight - total))
+
+        if (layoutElement) {
+            const isScrolledToBottom = layoutElement.scrollTop + layoutElement.clientHeight >= layoutElement.scrollHeight - 40;
+            if (isScrolledToBottom) {
+                console.log('已滑到底部');
+                setIsBottom(true);
+            }
         }
     }
 
@@ -23,7 +32,7 @@ export default function Layouts({ children, scrollBarHidden }) {
         }
     },[])
 
-    return <div className={scrollBarHidden ? styles.layoutsHidden : styles.layouts}>
+    return <div ref={layoutRef} className={scrollBarHidden ? styles.layoutsHidden : styles.layouts}>
         {children}
     </div>
 }
