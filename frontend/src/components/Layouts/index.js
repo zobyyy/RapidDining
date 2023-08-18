780d82b00f8a2a6f77869a2cdb39f3e6928b23e7
Zobyyy <codybabydd@gmail.com>
2023-08-18 09:17:46 +0800

    fix

--- frontend/src/components/Layouts/index.js
+++ frontend/src/components/Layouts/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './Layouts.module.scss'
 
 export default function Layouts({ children, scrollBarHidden }) {
-    const isBottom = false;
-    // const childrenWithProps = React.Children.map(children, child => 
-    //     React.cloneElement(child, isBottom)
-    // );
+    const containerRef = useRef(null);
+    const [isAtBottom, setIsAtBottom] = useState(false);
+
+    const handleScroll = () => {
+        const container = containerRef.current;
+        if (container) {
+            const isBottom = container.scrollHeight - container.scrollTop === container.clientHeight;
+            setIsBottom(isBottom);
+        }
+    }
+
     return <div className={scrollBarHidden ? styles.layoutsHidden : styles.layouts}>{children}</div>
 }
