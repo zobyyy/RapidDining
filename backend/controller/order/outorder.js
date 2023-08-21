d2bfd76f6a16cae3a6d3b2798ede890679a58ced
yihsuan <yiiihsuann@gmail.com>
2023-08-21 18:04:30 +0800

    feat order summary list API for restaurant to check

---
+++ backend/controller/order/outorder.js
@@ -0,0 +1,28 @@
+
+import { getOutOrderSummary } from '../../model/orderSummaryModel.js';
+
+// function ReqIsNumber(s) {
+//     return parseFloat(s).toString() !== "NaN"; 
+//   }
+
+export async function outOrderSummary(req, res) {
+  try {
+    //const { restaurantId } = req.body;
+    const { restaurantId,cursor } = req.query;
+    console.log('cursor in controller is',cursor);
+  
+    // if ((ReqIsNumber(tableId) && ReqIsNumber(reservationId))|| (!tableId && !reservationId)) {
+    //   return res.status(400).json({ error: 'Please Provide either tableId or reservationId.' });
+    // }
+
+    if (!restaurantId) {
+        return res.status(400).json({ error: 'Please Provide restaurantId.' });
+      }
+
+    const orderSummary = await getOutOrderSummary(restaurantId,cursor);
+    return res.status(200).json( {data:orderSummary.data} );
+  } catch (error) {
+    console.error('Error in getOrderSummary:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
