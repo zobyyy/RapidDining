353a1b44046536a1ff5748ebd9a923a5caed2bbb
yihsuan <yiiihsuann@gmail.com>
2023-08-21 17:28:18 +0800

    fix: add base 64 for resolving phone and change reseponse datas

--- backend/model/getorderModel.js
+++ backend/model/getorderModel.js
@@ -5,56 +5,90 @@ import { pool } from './util.js';
 export async function getPendingOrders(phone) {
   try {
     console.log(`the phone to be search is ${phone}`);
+    
+    const pendingOrders = [];
 
     const tableQuery = `
-  SELECT id FROM tableList
-   WHERE phone = ?;
- `;
+      SELECT t.id, t.restaurantId, r.name AS restaurantName, r.picture AS restaurantPicture
+      FROM tableList t
+      LEFT JOIN restaurant r ON t.restaurantId = r.id
+      WHERE t.phone = ?;
+    `;
     const [tableResult] = await pool.query(tableQuery, [phone]);
-    console.log(`search result =${tableResult}`);
-    const tableId = tableResult[0]?.id;
-    console.log(`search tableId = ${tableResult[0]?.id}`);
+    if (tableResult.length > 0) {
+      const tableInfo = tableResult[0];
+      pendingOrders.push({
+        tableId: tableInfo.id || null,
+        reservationId: null,
+        orderId: null,
+        status: null,
+        restaurantId: tableInfo.restaurantId || null,
+        restaurantName: tableInfo.restaurantName || null,
+        restaurantPic: tableInfo.restaurantPicture || null
+      });
+    }
+
+    const reservationQuery = `
+      SELECT r.id AS reservationId, r.restaurantId, res.name AS restaurantName, res.picture AS restaurantPicture
+      FROM Reservation r
+      LEFT JOIN restaurant res ON r.restaurantId = res.id
+      WHERE r.phone = ?;
+    `;
+    const [reservationResult] = await pool.query(reservationQuery, [phone]);
 
     
-    const reservationQuery = `
-    SELECT id, restaurantId FROM Reservation
-    WHERE phone = ?;
- `;
-    const [reservationResult] = await pool.query(reservationQuery, [phone]);
-    const reservationId = reservationResult[0]?.id;
-    const restaurantId = reservationResult[0]?.restaurantId;
-    console.log(`you have a reservation on res ${reservationResult[0]?.restaurantId}`);
+    if (reservationResult.length > 0) {
 
-
-    const orderQuery = `
-   SELECT id FROM OrderList
-   WHERE phone = ?;
- `;
-    const [orderResult] = await pool.query(orderQuery, [phone]);
-    const orderId = orderResult[0]?.id;
-
-    let reservationCount = null;
-
-    //status
-    if(restaurantId ){
-    const reservationCountQuery = `
+      let reservationCount = null;
+  
+      const reservationInfo = reservationResult[0];
+      
+      const reservationCountQuery = `
       SELECT COUNT(*) AS count FROM Reservation
       WHERE restaurantId = ? AND id < ?;
     `;
-
-    const [reservationCountResult] = await pool.query(reservationCountQuery, [restaurantId,reservationId]);
-    reservationCount = reservationCountResult[0]?.count + 1 ;
+    const [reservationCountResult] = await pool.query(reservationCountQuery, [reservationInfo.restaurantId
+      , reservationInfo.reservationId]);
+    reservationCount = reservationCountResult[0]?.count + 1;
     console.log(`you are the num ${reservationCount}`);
+
+      pendingOrders.push({
+        tableId: null,
+        reservationId: reservationInfo.reservationId || null,
+        orderId: null,
+        status: reservationCount ,
+        restaurantId: reservationInfo.restaurantId || null,
+        restaurantName: reservationInfo.restaurantName || null,
+        restaurantPic: reservationInfo.restaurantPicture || null
+      });
     }
 
-    return {
-      tableId: tableId || null,
-      reservationId: reservationId || null,
-      orderId: orderId || null,
-      status: reservationCount || null,
-    };
+    const orderQuery = `
+      SELECT o.id AS orderId, o.restaurantId, res.name AS restaurantName, res.picture AS restaurantPicture
+      FROM OrderList o
+      LEFT JOIN restaurant res ON o.restaurantId = res.id
+      WHERE o.phone = ?;
+
+    `;
+    const [orderResult] = await pool.query(orderQuery, [phone]);
+    if (orderResult.length > 0) {
+      const orderInfo = orderResult[0];
+      console.log('orderInfo result',orderResult[0]);
+      console.log('orderInfo restaurant result',orderResult[0].restaurantId);
+      pendingOrders.push({
+        tableId: null,
+        reservationId: null,
+        orderId: orderInfo.id || null,
+        status: null,
+        restaurantId: orderInfo.restaurantId || null,
+        restaurantName: orderInfo.restaurantName || null,
+        restaurantPic: orderInfo.restaurantPicture || null
+      });
+    }
+
+    return pendingOrders;
   } catch (error) {
-    console.error('Error in insertReservation:', error);
+    console.error('Error in getPendingOrders:', error);
     throw error;
   }
 }
