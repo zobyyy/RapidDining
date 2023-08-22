610f97f243c2a5b315b6aa2b8313b2ef7e696b4a
Zobyyy <codybabydd@gmail.com>
2023-08-22 10:37:31 +0800

    style(checkout page): create layout and index

---
+++ frontend/src/pages/Checkout/index.jsx
@@ -0,0 +1,10 @@
+import Layouts from '../../components/LayoutTablet'
+import styles from './Checkout.module.scss'
+
+export default function CheckoutPage() {
+  return (
+    <Layouts>
+        <div>111</div>
+    </Layouts>
+  )
+}
