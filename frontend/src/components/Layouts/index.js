f0a614bffc0dca2d82b24c5a895edc535a4b6bf0
tsaichiehhuang <a0970605512@gmail.com>
2023-08-22 17:45:58 +0800

    fix:刪infintie scroll & 購物車無商品預設文字

--- frontend/src/components/Layouts/index.js
+++ frontend/src/components/Layouts/index.js
@@ -1,36 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import styles from './Layouts.module.scss'
 
-export default function Layouts({ children, scrollBarHidden }) {
-    const layoutRef = useRef(null);
-    const [isBottom, setIsBottom] = useState(false);
-
-    const handleScroll = () => {
-        const layoutElement = layoutRef.current;
-        const bottomDistance = layoutElement.scrollHeight - (layoutElement.scrollTop + layoutElement.clientHeight);
-
-        console.log("bottom: "+isBottom)
-
-        if (layoutElement) {
-            if (bottomDistance < 40) {
-                console.log('滑到底了！');
-                setIsBottom(true);
-            }
-        }
-    }
-
-    useEffect(() => {
-        const layoutElement = layoutRef.current;
-        if (layoutElement) {
-            layoutElement.addEventListener('scroll', handleScroll);
-
-            return () => {
-                layoutElement.removeEventListener('scroll', handleScroll);
-            };
-        }
-    },[])
-
-    return <div ref={layoutRef} className={scrollBarHidden ? styles.layoutsHidden : styles.layouts}>
-        {children}
-    </div>
+export default function Layouts({ children }) {
+  return <div className={styles.layouts}>{children}</div>
 }
