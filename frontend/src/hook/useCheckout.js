20fce42c4857b8bdbada8e64d6b3f96062159473
tsaichiehhuang <a0970605512@gmail.com>
2023-08-23 17:11:09 +0800

    fix:checkout 結帳音效 & cart style

--- frontend/src/hook/useCheckout.js
+++ frontend/src/hook/useCheckout.js
@@ -5,8 +5,15 @@ import { useRouter } from 'next/router'
 const useCheckout = () => {
   const [updateOrderId, setUpdateOrderId] = useState(null)
   const router = useRouter()
+  const [audio, setAudio] = useState(null)
+
+  useEffect(() => {
+    setAudio(new Audio('/cash.mp3'))
+  }, [])
 
   const handleCheckout = async (orderId) => {
+    audio.play()
+
     try {
       const response = await fetch(
         `https://107.22.142.48/api/1.0/orders/${orderId}/checkout`,
@@ -22,7 +29,9 @@ const useCheckout = () => {
         const data = await response.json()
         console.log('hi', data.data.orders.id)
         setUpdateOrderId(data.data)
-        router.push('/OrderSubmit')
+        setTimeout(() => {
+          router.push('/OrderSubmit')
+        }, 50)
       }
     } catch (error) {
       console.error(error)
